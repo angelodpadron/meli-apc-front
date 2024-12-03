5bcb23b58ca30a2f3486f983afcff733f788cda1
Angelo Padron <padron891@gmail.com>
2024-12-03 11:05:04 -0300

    add pie chart component for metrics pages

--- src/app/views/admin/top-buyers-list/top-buyers-list.component.ts
+++ src/app/views/admin/top-buyers-list/top-buyers-list.component.ts
@@ -15,6 +15,8 @@ import {
 } from '@angular/material/table';
 import {CurrencyPipe} from '@angular/common';
 import {PieChartModule} from "@swimlane/ngx-charts";
+import {PieChartComponent} from "../../../shared/pie-chart/pie-chart.component";
+import {typewriter} from "../../../shared/utils/typewriter";
 
 @Component({
   selector: 'app-top-buyers-list',
@@ -32,32 +34,39 @@ import {PieChartModule} from "@swimlane/ngx-charts";
     MatHeaderCellDef,
     CurrencyPipe,
     PieChartModule,
+    PieChartComponent,
   ],
   templateUrl: './top-buyers-list.component.html',
   styleUrl: './top-buyers-list.component.css',
 })
 export class TopBuyersListComponent implements OnInit {
   topBuyers: UserPurchaseCount[] = [];
+  data: any[] = [];
   displayedColumns: string[] = [
     'position',
     'email',
     'total_spend',
     'total_items',
   ];
+  emptyListMessage: string = ""
 
-  // chart test
-  data: any[] = [];
-  view: [number, number] = [700, 400];
-
-  constructor(private adminService: AdminService) {}
+  constructor(private adminService: AdminService) {
+  }
 
   ngOnInit(): void {
     this.adminService.topFiveBuyers().subscribe({
       next: (value) => {
         this.topBuyers = value
         this.data = value.map((entry) => ({name: entry.email, value: entry.total_items}))
+        if (!value.length) setTimeout(() => this.initEmptyListMessage(), 1000)
       },
     });
-
   }
+
+  private initEmptyListMessage() {
+    const setter = (value: string) => this.emptyListMessage = value
+    typewriter("Aparentemente, nadie guardo un producto (¬_¬)", setter)
+  }
+
+
 }
